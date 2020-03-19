65c03983fd86f7013c076ab7493a5627d69cdfee
Gatien BOVYN <gbovyn@wemanity.com>
2020-03-19 22:19:38 +0100

    refactor: extract common test logic

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -3,38 +3,28 @@ let expect = chai.expect;
 
 let validator = require('../lib/validator');
 
+let expectedToIncludeErrorWhenInvalid = (n, error) => {
+    expect(validator(n)).to.include(error);
+};
+
 describe('A validator', () => {
-    it('should return no errors for valid numbers', () => {
-        expect(validator(7)).to.be.empty;
-    });
+    it('should return no errors for valid numbers', () => expect(validator(7)).to.be.empty);
 
     describe('should include error.nonpositive for not strictly positive numbers', () => {
-        it('like 0', () => {
-            expect(validator(0)).to.include('error.nonpositive');
-        });
+        it('like 0', () => expectedToIncludeErrorWhenInvalid(0, 'error.nonpositive'));
 
-        it('like -2', () => {
-            expect(validator(-2)).to.include('error.nonpositive');
-        });
+        it('like -2', () => expectedToIncludeErrorWhenInvalid(-2, 'error.nonpositive'));
     });
 
     describe('should include error.three for divisible by 3 numbers', () => {
-        it('like 3', () => {
-            expect(validator(3)).to.include('error.three');
-        });
+        it('like 3', () => expectedToIncludeErrorWhenInvalid(3, 'error.three'));
 
-        it('like 15', () => {
-            expect(validator(15)).to.include('error.three');
-        });
+        it('like 15', () => expectedToIncludeErrorWhenInvalid(15, 'error.three'));
     });
 
     describe('should include error.five for divisible by 5 numbers', () => {
-        it('like 5', () => {
-            expect(validator(5)).to.include('error.five');
-        });
+        it('like 5', () => expectedToIncludeErrorWhenInvalid(5, 'error.five'));
 
-        it('like 15', () => {
-            expect(validator(15)).to.include('error.five');
-        });
+        it('like 15', () => expectedToIncludeErrorWhenInvalid(15, 'error.five'));
     });
 });
