f26907ecfa7db56beb63267a3b285e46cfe5c1a6
Gatien BOVYN <gbovyn@wemanity.com>
2020-03-19 17:39:50 +0100

    refactor: Remove repetition in nonpositive testing cases

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -7,13 +7,14 @@ describe('A validator', () => {
     it('should return no errors for valid numbers', () => {
         expect(validator(3)).to.be.empty;
     });
-    
-    it('should return error.nonpositive for not strictly positive numbers, like 0', () => {
-        expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
-    });
 
-    it('should return error.nonpositive for not strictly positive numbers, like -2', () => {
-        expect(validator(-2)).to.be.deep.equal(['error.nonpositive']);
+    describe('should return error.nonpositive for not strictly positive numbers', () => {
+        it('like 0', () => {
+            expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
+        });
+
+        it('like -2', () => {
+            expect(validator(-2)).to.be.deep.equal(['error.nonpositive']);
+        });
     });
-    
 });
