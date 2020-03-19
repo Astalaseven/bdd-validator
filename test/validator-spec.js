163e450a0d3a706cf0c252e5de77f83b5e1de68e
Gatien BOVYN <gbovyn@wemanity.com>
2020-03-19 17:44:43 +0100

    feat: Handle validation of divisible by 3 numbers

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -5,7 +5,7 @@ let validator = require('../lib/validator');
 
 describe('A validator', () => {
     it('should return no errors for valid numbers', () => {
-        expect(validator(3)).to.be.empty;
+        expect(validator(7)).to.be.empty;
     });
 
     describe('should return error.nonpositive for not strictly positive numbers', () => {
@@ -15,6 +15,16 @@ describe('A validator', () => {
 
         it('like -2', () => {
             expect(validator(-2)).to.be.deep.equal(['error.nonpositive']);
+        });
+    });
+
+    describe('should return error.three for divisible by 3 numbers', () => {
+        it('like 3', () => {
+            expect(validator(3)).to.be.deep.equal(['error.three']);
+        });
+
+        it('like 6', () => {
+            expect(validator(6)).to.be.deep.equal(['error.three']);
         });
     });
 });
