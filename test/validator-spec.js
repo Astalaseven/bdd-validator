1b2a7fb86d4827c525e82101a5df87a8fd504422
Gatien BOVYN <gbovyn@wemanity.com>
2020-03-19 17:20:09 +0100

    feat: Implement valid number logic

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -4,6 +4,10 @@ let expect = chai.expect;
 let validator = require('../lib/validator');
 
 describe('A validator', () => {
+    it('should return no errors for valid numbers', () => {
+        expect(validator(3)).to.be.empty;
+    });
+    
     it('should return error.nonpositive for not strictly positive numbers', () => {
         expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
     });
