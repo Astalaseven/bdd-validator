023e5488bfc45f828ea5b7a5da93409da28f4ad9
Gatien BOVYN <gbovyn@wemanity.com>
2020-03-19 17:22:08 +0100

    feat: Implement case for strictly negative number

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -8,7 +8,12 @@ describe('A validator', () => {
         expect(validator(3)).to.be.empty;
     });
     
-    it('should return error.nonpositive for not strictly positive numbers', () => {
+    it('should return error.nonpositive for not strictly positive numbers, like 0', () => {
         expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
     });
+
+    it('should return error.nonpositive for not strictly positive numbers, like -2', () => {
+        expect(validator(-2)).to.be.deep.equal(['error.nonpositive']);
+    });
+    
 });
