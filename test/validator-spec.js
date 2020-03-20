f1f6932d9de1c0db78cdf0f0c115a5f237d2dccf
Gatien BOVYN <gbovyn@wemanity.com>
2020-03-20 08:02:12 +0100

    refactor: use close to choose which validation rules are used
    use `context` instead of `describe` to define scenarios - describe is more fit to define features and actions

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -1,30 +1,44 @@
 let chai = require('chai');
 let expect = chai.expect;
 
-let validator = require('../lib/validator');
+let validatorWith = require('../lib/validator');
+let nonPositiveValidationRule = require('../lib/rules/nonPositive');
+let nonDivisibleValidationRule = require('../lib/rules/nonDivisible');
 
-let expectedToIncludeErrorWhenInvalid = (n, error) => {
-    expect(validator(n)).to.include(error);
-};
+describe('A validation', () => {
+    let validator;
 
-describe('A validator', () => {
-    it('should return no errors for valid numbers', () => expect(validator(7)).to.be.empty);
+    context('with default rules', () => {
+        beforeEach(() => {
+            validator = validatorWith([
+                nonPositiveValidationRule,
+                nonDivisibleValidationRule(3, 'error.three'),
+                nonDivisibleValidationRule(5, 'error.five')
+            ]);
+        });
 
-    describe('should include error.nonpositive for not strictly positive numbers', () => {
-        it('like 0', () => expectedToIncludeErrorWhenInvalid(0, 'error.nonpositive'));
+        it('should return no errors for valid numbers', () => expect(validator(7)).to.be.empty);
 
-        it('like -2', () => expectedToIncludeErrorWhenInvalid(-2, 'error.nonpositive'));
+        context('should include error.nonpositive for not strictly positive numbers', () => {
+            it('like 0', () => expectedToIncludeErrorWhenInvalid(0, 'error.nonpositive'));
+
+            it('like -2', () => expectedToIncludeErrorWhenInvalid(-2, 'error.nonpositive'));
+        });
+
+        context('should include error.three for numbers divisible by 3', () => {
+            it('like 3', () => expectedToIncludeErrorWhenInvalid(3, 'error.three'));
+
+            it('like 15', () => expectedToIncludeErrorWhenInvalid(15, 'error.three'));
+        });
+
+        context('should include error.five for numbers divisible by 5', () => {
+            it('like 5', () => expectedToIncludeErrorWhenInvalid(5, 'error.five'));
+
+            it('like 15', () => expectedToIncludeErrorWhenInvalid(15, 'error.five'));
+        });
     });
 
-    describe('should include error.three for divisible by 3 numbers', () => {
-        it('like 3', () => expectedToIncludeErrorWhenInvalid(3, 'error.three'));
-
-        it('like 15', () => expectedToIncludeErrorWhenInvalid(15, 'error.three'));
-    });
-
-    describe('should include error.five for divisible by 5 numbers', () => {
-        it('like 5', () => expectedToIncludeErrorWhenInvalid(5, 'error.five'));
-
-        it('like 15', () => expectedToIncludeErrorWhenInvalid(15, 'error.five'));
-    });
+    let expectedToIncludeErrorWhenInvalid = (n, error) => {
+        expect(validator(n)).to.include(error);
+    };
 });
